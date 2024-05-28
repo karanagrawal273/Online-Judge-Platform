845b5b9341ba537d8c90b64768bc6ff58c8a365f
Karan Agrawal <agrawalkaran273@gmail.com>
2024-05-28 21:15:09 +0530

    Added user authorization in CRUD features

--- client/src/pages/AddProblem.jsx
+++ client/src/pages/AddProblem.jsx
@@ -1,8 +1,28 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "../css/AddProblem.css";
 const AddProblem = (props) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const verifyCookie = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:5000",
+          {},
+          { withCredentials: true }
+        );
+        if (!response.data.success) {
+          console.log("token not found Please login first");
+          navigate("/login");
+        }
+      } catch (error) {
+        console.log(error.response.data.message);
+        navigate("/login");
+      }
+    };
+    verifyCookie();
+  }, []);
   const id = useParams().id;
   // console.log(id);
   const [values, setValues] = useState({
@@ -11,7 +31,6 @@ const AddProblem = (props) => {
     difficulty: "",
   });
   const { title, statement, difficulty } = values;
-  const navigate = useNavigate();
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setValues({
@@ -65,14 +84,17 @@ const AddProblem = (props) => {
             <label className="form-label" htmlFor="difficulty">
               Difficulty:
             </label>
-            <input
-              className="form-input"
-              type="text"
+            <select
+              className="form-select"
               name="difficulty"
               value={difficulty}
-              placeholder="Enter difficulty"
               onChange={handleOnChange}
-            />
+            >
+              <option value="">Select difficulty</option>
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
           </div>
           <button className="submit-button" type="submit">
             Submit
--- client/src/pages/DeleteProblem.jsx
+++ client/src/pages/DeleteProblem.jsx
@@ -1,18 +1,34 @@
 import axios from "axios";
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../css/DeleteProblem.css";
 const DeleteProblem = (props) => {
   const { _id, title, statement, difficulty } = props.obj;
+  const navigate = useNavigate();
   const deleteProblem = async () => {
     try {
-      const response = await axios.delete(
-        `http://localhost:5000/problems/${_id}`
+      const response = await axios.post(
+        "http://localhost:5000",
+        {},
+        { withCredentials: true }
       );
-      console.log("Problem gets deleted");
-      window.location.reload();
+      // console.log(response);
+      if (!response.data.success) {
+        console.log("token not found Please login first");
+        navigate("/login");
+      } else {
+        try {
+          const response = await axios.delete(
+            `http://localhost:5000/problems/${_id}`
+          );
+          console.log("Problem gets deleted");
+          window.location.reload();
+        } catch (error) {
+          console.log(error.response.data.message);
+        }
+      }
     } catch (error) {
-      console.log(error.response.data.message);
+      console.log("Some error occurred");
     }
   };
   return (
